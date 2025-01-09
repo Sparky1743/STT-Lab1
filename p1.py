2eee95f67d43e3411559ccf7462daa01f69e784d
Sparky1743 <birudugadda.srivibhav@iitgn.ac.in>
2025-01-09 10:36:00 +0530

    modified p1.py

--- p1.py
+++ p1.py
@@ -1,1 +1,32 @@
-print("Hello world")
+# Grade Caluculator
+
+# This function is used to calculate your grade.
+def grade_calculator(marks):
+    if marks >= 90:
+        return "A+"
+    elif marks >= 80:
+        return "A"
+    elif marks >= 70:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    elif marks >= 50:
+        return "D"
+    else:
+        return "F"
+
+# This function is used to print your grade in terminal.
+def print_your_grade(marks):
+    
+    if (grade_calculator(marks) == "F"):
+        print("You have been failed.")
+        print("Please meet your faculty advisor.")
+
+    else:
+        # prints the grade
+        print(f"Your grade is {grade_calculator(marks)}")
+
+# User input
+marks = int(input("Enter your marks: "))
+# Run the function 
+print_your_grade(marks)
